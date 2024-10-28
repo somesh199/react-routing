c4c37fac0f454f62e1d32469559110b31cfeee19
SomeshSharma <sharmasomesh278@gmail.com>
2024-10-28 19:47:21 +0530

    added project files

---
+++ frontend/src/pages/EditEvent.js
@@ -0,0 +1,11 @@
+import { useRouteLoaderData } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
+function EditEvent() {
+    const data = useRouteLoaderData('event-detail');
+    return(
+        <EventForm event={data.event} method="PATCH"></EventForm>
+    );
+}
+
+export default EditEvent;
